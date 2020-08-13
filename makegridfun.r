d734832eec56a6453bae74efa0bf9039307655aa
rungec <runge@nceas.ucsb.edu>
2020-08-13 12:34:46 +1000

    forgot to add cell dimension to function

--- makegridfun.r
+++ makegridfun.r
@@ -6,7 +6,7 @@ makegridfun <- function(bb, currdiameter){
   
   bb_buff <- st_buffer(bb, currdiameter)
   
-  currgrid <- st_make_grid(bb, square=FALSE, flat_topped = TRUE)
+  currgrid <- st_make_grid(bb, cellsize=cell_diameter, square=FALSE, flat_topped = TRUE)
   
   return(currgrid)
 }
