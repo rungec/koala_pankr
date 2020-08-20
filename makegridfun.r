331c5ba8c1e929b81892eaa1fee14d2e58abd216
rungec <runge@nceas.ucsb.edu>
2020-08-21 09:01:23 +1000

    added cellid to grid

--- makegridfun.r
+++ makegridfun.r
@@ -7,6 +7,7 @@ makegridfun <- function(bb, currdiameter){
   bb_buff <- st_buffer(bb, currdiameter)
   
   currgrid <- st_make_grid(bb, cellsize=cell_diameter, square=FALSE, flat_topped = TRUE)
+  currgrid <- currgrid %>% bind_cols(cellid = c(1:nrow(currgrid))) %>% st_sf()
   
   return(currgrid)
 }
