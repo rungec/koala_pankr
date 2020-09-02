c5d23c552ad4983f211ba098bd0da5773d712b05
rungec <runge@nceas.ucsb.edu>
2020-09-03 09:57:35 +1000

    added 99perc, made script more flexible

--- Climate_refugia.r
+++ Climate_refugia.r
@@ -9,12 +9,12 @@ require(tidyverse)
 require(raster)
 
 #Dirs
-setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/Output")
+setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/Data_inp")
 climdir <- "D:/Box Sync/GPEM_Postdoc/Koala_NESP/04_Datasets/Refugia/Climate_change"
-threshdir <- "/Climrasters_thresholded"
+threshdir <- "Climrasters_thresholded/"
 
 #load threshold table
-thresholds <- read.csv("Climate_thresholds.csv")
+thresholds <- read.csv(paste0(threshdir, "Climate_thresholds.csv"))
 
 #maxent and nichemapper predictions have different extents (nichemapper includes tasmania), so we load separately
 maxestack <- stack(list.files(paste0(climdir, "/Maxent"), ".tif", recursive = TRUE, full.names = TRUE))
@@ -30,7 +30,7 @@ reclassfun <- function(currstack, modtype, threshcol) {
   for (d in c("All_records", "No_duplicates")){
     for (i in 1:nlayers(currstack)){
       
-      dir.create(paste0("Climrasters_thresholded/", threshcol), showWarnings = FALSE)
+      dir.create(paste0(threshdir, threshcol), showWarnings = FALSE)
       
       currname <- names(currstack)[i]
       if (str_detect(currname, "mod")) {
@@ -43,7 +43,7 @@ reclassfun <- function(currstack, modtype, threshcol) {
 
       val <- thresholds[which(thresholds$Scenario==currmod & thresholds$Records==d), threshcol][[1]]
       rcltbl <- matrix(c(0, val, 0, val, 1, 1), ncol=3, byrow=TRUE)
-      reclassify(currstack[[i]], rcltbl, filename=paste0("Climrasters_thresholded/", threshcol, "/", modtype, "_", d, "_", currname), format='GTiff', datatype='INT2S')
+      reclassify(currstack[[i]], rcltbl, filename=paste0(threshdir, threshcol, "/", modtype, "_", d, "_", currname), format='GTiff', datatype='INT2S')
       
     }}}
 
@@ -59,7 +59,7 @@ reclassfun(maxestack, "Maxent", "perc50ofrecords")
 #Combine the thresholded rasters to map refugia
 
 refugiaFun <- function(threshcol, currtime, dups){
-  rastlist <- list.files(paste0("Climrasters_thresholded/", threshcol, "/"), dups, recursive = FALSE, full.names = TRUE)
+  rastlist <- list.files(paste0(threshdir, "/", threshcol, "/"), pattern=dups, recursive = FALSE, full.names = TRUE)
   currstack <- raster::stack(rastlist)
   currlist <- names(currstack) %>% as_tibble %>%  
                           mutate(Time = case_when(str_detect(value, "2070") ~"2070",
@@ -69,7 +69,7 @@ refugiaFun <- function(threshcol, currtime, dups){
                           filter(Time==currtime)
   currstack <- currstack[[which(names(currstack) %in% currlist$value)]]
   curroup <- sum(currstack)
-  writeRaster(curroup, filename=paste0("Climate_refugia/Refugia_", currtime, "_", threshcol, "_", dups,".tif"), format='GTiff', datatype='INT2S')
+  writeRaster(curroup, filename=paste0(threshdir, "/Climate_refugia/Refugia_", currtime, "_", threshcol, "_", dups,".tif"), format='GTiff', datatype='INT2S')
 } 
 
 #High suitability (80% of spatially sampled records fall within threshold)
@@ -81,7 +81,9 @@ refugiaFun("perc90ofrecords", "Current", "No_duplicates")
 #Low suitability (95% of spatially sampled records fall within threshold)
 refugiaFun("perc95ofrecords", "2070", "No_duplicates")
 refugiaFun("perc95ofrecords", "Current", "No_duplicates")
-
+#unfiltered rasters
+refugiaFun("perc99ofrecords", "2070", "No_duplicates")
+refugiaFun("perc99ofrecords", "Current", "No_duplicates")
 
 #Just rename the files and move to another directory
 renamefun <- function(currstack, modtype) {
@@ -96,7 +98,7 @@ renamefun <- function(currstack, modtype) {
       currmod <- paste0(str_split(currname, "_")[[1]][2:3], collapse="_")
     }
     
-    writeRaster(currstack[[i]], filename=paste0("Climrasters_thresholded/nothreshold/", modtype, "_", currname), format='GTiff', datatype='FLT4S')
+    writeRaster(currstack[[i]], filename=paste0(threshdir, "nothreshold/", modtype, "_", currname), format='GTiff', datatype='FLT4S')
   }
 } 
 renamefun(nichestack, "NicheMapper")
