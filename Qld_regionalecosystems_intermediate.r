5234f672a403a1be842f08127ddaa711b0d6c1c0
rungec <runge@nceas.ucsb.edu>
2020-10-09 10:27:10 +1000

    calculate RE utility

--- Qld_regionalecosystems_intermediate.r
+++ Qld_regionalecosystems_intermediate.r
@@ -5,24 +5,44 @@ library(tidyverse)
 
 setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/06_Analysis/Sofia/Sofia_processed")
 
-re_shp <- st_read("Maps_graphs/Shapefiles/biome_re_tree_uti_area/biome_re_tree_uti_area.shp") 
-re_shp <- mutate(re_shp, RE1_dup = as.character(RE1))
+re_shp <- st_read("Shapefiles/biome_re_tree_uti_area/biome_re_tree_uti_area.shp") 
 
 kocc_df <- read.csv("REDD_QldnoSEQ_summary_of_koala_occurrence_forexperts.csv", header=TRUE) 
-kocc_df <- mutate(kocc_df, re_id_dup = as.character(re_id))
+
+#############################
+#Fix text in shp
+
+re_shp <- re_shp %>% mutate(Biome = case_when(Biome == "Nandewar" ~ "New England Tablelands",
+                                              Biome == "Central Mackay Coast" ~ "Central Queensland Coast",
+                                              Biome == "Darling Riverine Plains" ~ "Mulga Lands",
+                                              Biome == "Brigalow Belt South" ~ "Brigalow Belt",
+                                              Biome == "Brigalow Belt North" ~ "Brigalow Belt",
+                                              Biome == "South Eastern Queensland" ~ "South East Queensland",
+                                              TRUE ~ Biome),
+                            Spcs_tl = case_when(Spcs_tl=="Other" ~ "NA",
+                                                TRUE ~ Spcs_tl),
+                            RE1 = case_when(RE1 == "on-rem" ~ "non-rem",
+                                            RE1 =="stuary" ~ "estuary",
+                                            RE1 =="anal" ~ "canal",
+                                            RE1 =="cean" ~ "ocean",
+                                            RE1 =="cean" ~ "ocean",
+                                            RE1 =="snd" ~ "sand",
+                                            RE1 =="wter" ~ "water",
+                                            RE1 =="sallow" ~ "shallow",
+                                            TRUE ~ RE1))
+re_shp <- re_shp %>% rename("Tree_utility" = "Spcs_tl", "n_ktreesp" = "Spcs_t_")
+
+re_shp <- re_shp %>% select(!OBJECTI:SQ_KM)
 
 #############################
 #Write preliminary koala tree map
 re_oup <- left_join(re_shp, kocc_df, by=c("RE1"="re_id"))
-re_oup <- re_oup %>% rename("Tree_utility" = "Spcs_tl")
-re_oup <- re_oup %>% rename("n_ktreesp" = "Spcs_t_")
 re_oup <- re_oup %>% mutate(RE_suitability = case_when(Tree_utility=='Higher' & perc_REpolys_withkoala > 1.0 ~ "Very high", 
                                                        Tree_utility =="Medium" & perc_REpolys_withkoala > 1.0 ~ "High",
                                                        Tree_utility %in% c('Higher', "Medium") & perc_REpolys_withkoala > 0 ~ "Medium",
                                                        Tree_utility %in% c('Higher', "Medium") & is.na(n_koalaocc) ~ "Low",
                                                        TRUE ~ "None"))
 
-re_oup <- re_oup %>% select(!REG_NAM)
 re_oup <- re_oup %>% select(!Description)
 #st_write(re_oup, "Shapefiles/biome_re_utility/Qld_RE_utility.gpkg", append=FALSE)
 st_write(re_oup, "Shapefiles/biome_re_utility/Qld_RE_utility.shp")
@@ -31,8 +51,8 @@ st_write(re_oup, "Shapefiles/biome_re_utility/Qld_RE_utility.shp")
 #Write merged .csv for experts to evaluate
 re_df <- re_shp %>% st_set_geometry(NULL) 
 re_df <- re_df %>% group_by(RE1) %>% summarise(Biome = first(Biome), 
-                                            Tree_utility = first(Spcs_tl),
-                                            Num_utilised_trees = first(Spcs_t_))
+                                            Tree_utility = first(Tree_utility),
+                                            n_ktreesp = first(n_ktreesp))
 
 re_merge <- full_join(kocc_df, re_df, by=c("re_id" = "RE1"))
 re_merge <- re_merge %>% mutate(RE_suitability = case_when(Tree_utility=='Higher' & perc_REpolys_withkoala > 1.0 ~ "Very high", 
