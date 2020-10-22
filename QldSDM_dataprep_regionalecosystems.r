d0613dfd541d54140557cf2642d7191311980bc4
rungec <runge@nceas.ucsb.edu>
2020-10-22 14:46:23 +1000

    added historical occurences

--- QldSDM_dataprep_regionalecosystems.r
+++ QldSDM_dataprep_regionalecosystems.r
@@ -11,12 +11,15 @@ require(tidyverse)
 #Dirs
 setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/Output")
 REdir <- "D:/Box Sync/GPEM_Postdoc/Koala_NESP/04_Datasets/Vegetation/Qld_vege_mapping/Regional_ecosystem_v11/"
-occdir <- "D:/Box Sync/GPEM_Postdoc/Koala_NESP/04_Datasets/Occurrence records"
+occdir <- "D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/Data_inp/"
 
 #Load data
 #current koala sightings
-currkoala <- st_read(occdir, "Koala_Qld_noSEQ_2000on_1kmres_noDup")
+currkoala <- st_read(paste0(occdir, "Koala_Qld_noSEQ_2000on_1kmres_noDup.shp"))
 st_crs(currkoala) <- CRS("+init=epsg:4283") #set proj as GDA_94 EPSG4283
+#current koala sightings
+histkoala <- st_read(paste0(occdir, "Koala_Qld_noSEQ_1970to2000_1kmres_noDup.shp"))
+st_crs(histkoala) <- CRS("+init=epsg:4283") #set proj as GDA_94 EPSG4283
 
 #Load REDD lookup table
 redd <- read_csv(paste0(REdir, 'redd-v11-1-2019.csv'))
@@ -36,8 +39,13 @@ reveg$oid <- 1:nrow(reveg)
 currkoala <- st_transform(currkoala, 3577)
 currkb <- st_buffer(currkoala, 1000)
 currkb <- st_transform(currkb, 4283)
-
 overlapkv <- st_intersects(currkb, reveg, sparse=TRUE)
+
+histkoala <- st_transform(histkoala, 3577)
+histkb <- st_buffer(histkoala, 1000)
+histkb <- st_transform(histkb, 4283)
+
+overlapkvh <- st_intersects(histkb, reveg, sparse=TRUE)
 
 
 #Join to occurrence and RE attribute tables
@@ -50,8 +58,19 @@ for(i in 1:length(overlapkv)){
   tb <- rbind(tb, cbind(currpt, currveg))
 }
 
+tbh <- data.frame()
+for(i in 1:length(overlapkvh)){
+  currset <- overlapkvh[[i]]
+  currveg <- st_drop_geometry(reveg[currset, c("oid", "RE_LABEL", "PC_LABEL", "SHAPE_Area")])
+  currpt <- st_drop_geometry(histkb[i, c("OBJECTID", "START_DATE", "LATITUDE", "LONGITUDE")])
+  currpt <- do.call("rbind", replicate(nrow(currveg), currpt, simplify = FALSE)) #repeat each row n times
+  tbh <- rbind(tbh, cbind(currpt, currveg))
+}
+
 #Split out RE1 in format matching REDD table
 tb <- tb %>% mutate(RE1 = case_when(str_detect(RE_LABEL, "/") ~ str_extract(RE_LABEL, "[^//]+"),
+                                     TRUE ~ RE_LABEL))
+tbh <- tbh %>% mutate(RE1 = case_when(str_detect(RE_LABEL, "/") ~ str_extract(RE_LABEL, "[^//]+"),
                                      TRUE ~ RE_LABEL))
 
 #number of polygons for each RE
@@ -65,18 +84,22 @@ Num_RE_polys <- reveg %>% st_drop_geometry() %>%
 #number of koala records in each RE
 summarytb <- tb %>% group_by(RE1) %>% summarise(n_koalaocc = n_distinct(OBJECTID),
                                                         n_REpolys_withkoala = n_distinct(oid)) 
+summarytbh <- tbh %>% group_by(RE1) %>% summarise(n_koalaocchist = n_distinct(OBJECTID),
+                                                        n_REpolys_withkoalahist = n_distinct(oid)) 
 
 #join summaries to the redd table
 redd_oup <- left_join(redd, Num_RE_polys, by = c("re_id" = "REfirst"))
 redd_oup <- left_join(redd_oup, summarytb, by = c("re_id" = "RE1"))
-redd_oup <- redd_oup %>% mutate(perc_REpolys_withkoala = 100*n_REpolys_withkoala/n_RE_polys) 
+redd_oup <- left_join(redd_oup, summarytbh, by = c("re_id" = "RE1"))
+redd_oup <- redd_oup %>% mutate(perc_REpolys_withkoala = 100*n_REpolys_withkoala/n_RE_polys,
+                                perc_REpolys_withkoala_hist = 100*n_REpolys_withkoalahist/n_RE_polys) 
                           
 #subset out the REs found in the study region #and that might be associated with koala
-redd_oup2 <- redd_oup %>% select(re_id, Description, Euc_present, n_RE_polys, n_koalaocc, n_REpolys_withkoala, perc_REpolys_withkoala ) %>% 
+redd_oup2 <- redd_oup %>% select(re_id, Description, Euc_present, n_RE_polys, n_koalaocc, n_REpolys_withkoala, perc_REpolys_withkoala, n_REpolys_withkoalahist, perc_REpolys_withkoala_hist) %>% 
                          # filter(n_RE_polys > 0 & Euc_present=='Euc' | n_koalaocc>0 )
                           filter(n_RE_polys > 0 )
 
 #Save
-write_csv(redd_oup, "REDD_QldnoSEQ_summary_of_koala_occurrence.csv")
-write_csv(redd_oup2, "REDD_QldnoSEQ_summary_of_koala_occurrence_forexperts.csv")
+write_csv(redd_oup, "REDD_QldnoSEQ_summary_of_koala_occurrence_v3.csv")
+write_csv(redd_oup2, "REDD_QldnoSEQ_summary_of_koala_occurrence_forexperts_v3.csv")
 
