bfc40ca2dc63f63776aad6eaaec20557ee1cd241
rungec <runge@nceas.ucsb.edu>
2020-11-25 15:39:15 +1000

    modified to join new qld habitat layer that uses historical records

--- join_extracted_data.r
+++ join_extracted_data.r
@@ -37,82 +37,58 @@ write.csv(df, "Output/Gridded_data/koala_gridded_vars_100ha_SEQ.csv", row.names=
 k_grid <- left_join(k_grid, df, by='cellid')
 
 ###############################
-#Regional ecosytem suitability 1 & 2
-setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/")
-oupdir <- "Output/Gridded_data/clean/"
-cell_area = "100ha" 
-
-load(paste0(oupdir, "koala_gridded_vars_100ha_tidy.Rdata"))
-
-re_1 <- st_read(dsn="C:/Users/Claire/Documents/ArcGIS/Default.gdb", layer="koala_re_qld_Union_1")
-
-re_1 <- re_1 %>% filter(!cellid==0) %>% st_set_geometry(NULL)
-
-re_1_group <- re_1 %>% mutate(habitat = case_when(FID_Qld_RE_utility_Dissolve_Proj1_singlepart == -1 ~ 0,
-                                                  TRUE ~ 1))
-re_1_group <- re_1_group %>% filter(habitat == 1) %>% 
-  group_by(cellid) %>% 
-  summarise(re_suitable_1_ha_qld = round(sum(Shape_Area)/10000, 0)) %>%
-  ungroup()
-
-re_1_group <- re_1_group %>% select(cellid, re_suitable_1_ha_qld)
-rm(re_1)
-
-re_12 <- st_read(dsn="C:/Users/Claire/Documents/ArcGIS/Default.gdb", layer="koala_re_qld_Union_12")
-
-re_12 <- re_12 %>% filter(!cellid==0) %>% st_set_geometry(NULL)
-
-re_12_group <- re_12 %>% mutate(habitat = case_when(FID_Qld_RE_utility_Dissolve_Proj12_singlepart == -1 ~ 0,
-                                                    TRUE ~ 1))
-re_12_group <- re_12_group %>% filter(habitat == 1) %>% 
-  group_by(cellid) %>% 
-  summarise(re_suitable_12_ha_qld = round(sum(Shape_Area)/10000, 0)) %>%
-  ungroup()
-
-re_12_group <- re_12_group %>% select(cellid, re_suitable_12_ha_qld)
-rm(re_12)
-
-k_fix <- left_join(k_fix, re_1_group, by='cellid')
-k_fix <- left_join(k_fix, re_12_group, by='cellid')
-
-###############################
-#Regional ecosytem suitability 3
+#Regional ecosytem suitability 1-3
 setwd("D:/Box Sync/GPEM_Postdoc/Koala_NESP/07_Processing/Output/Gridded_data/")
 oupdir <- "clean/"
+
 
 load(paste0(oupdir, "koala_gridded_vars_100ha_tidy.Rdata"))
 
 k_tmp <- k_fix %>% st_set_geometry(NULL) %>% select(cellid)
 
 joinfun <- function(biome){
-  re <- st_read(dsn="intermediate/re_biome.gdb", layer=biome)
+  for(i in 1:3){
+  if(biome=="Brigalow_Belt"){
+  re <- st_read(paste0("D:/Box Sync/GPEM_Postdoc/Koala_NESP/06_Analysis/Sofia/Qld_habitat/Task1/", biome, "_RE", i, "_PU.shp"))
+  } else {
+  re <- st_read(paste0("D:/Box Sync/GPEM_Postdoc/Koala_NESP/06_Analysis/Sofia/Qld_habitat/Task1/", biome, "_PU_RE", i, "_PU.shp")) 
+  }
   re <- re %>% st_set_geometry(NULL) %>% 
     filter(!cellid==0) %>% 
-    filter(habitat==1) %>%
-    select(cellid, habitat, "SUM_AREA_GEO")
-  print(max(re$SUM_AREA_GEO, na.rm=TRUE))
+    filter(habitat==1) 
+  names(re)[grep("Area", names(re))] <- "Area_ha"
+    re <- re %>% select(cellid, habitat, "Area_ha") %>% 
+          group_by(cellid) %>% summarise(habitat = max(habitat),
+                                          Area_ha = sum(Area_ha))
+  print(max(re$Area_ha, na.rm=TRUE))
   k_tmp <- k_tmp %>% left_join(re, by='cellid')
-  names(k_tmp)[ncol(k_tmp)] <- paste0("re_3_", biome)
+  names(k_tmp)[(ncol(k_tmp)-1):ncol(k_tmp)] <- c(paste0("re", i, "_habitat_", biome), paste0("re", i, "_area_", biome))
+  }
   return(k_tmp)
 }
 
 
-k_tmp <- joinfun("brigalow")
-k_tmp <- joinfun("centralqldcoast")
-k_tmp <- joinfun("desertup")
-k_tmp <- joinfun("eineup")
-k_tmp <- joinfun("mitchell")
-k_tmp <- joinfun("mulga")
-k_tmp <- joinfun("neweng")
+k_tmp <- joinfun("Brigalow_Belt")
+k_tmp <- joinfun("Central_QC")
+k_tmp <- joinfun("Desert_Uplands")
+k_tmp <- joinfun("Einasleigh_Uplands")
+k_tmp <- joinfun("Mitchell_Grass")
+k_tmp <- joinfun("Mulga_Lands")
+k_tmp <- joinfun("New_England")
+k_tmp <- joinfun("Wet_trop")
 k_tmp <- joinfun("seq")
-k_tmp <- joinfun("wettrop")
 
-k_agg <- k_tmp %>% rowwise() %>% mutate(re_suitable_3_ha_qld = sum(across(contains("re_3")), na.rm=TRUE),
-                                        habitat_present_3_qld = sum(across(contains("habitat")), na.rm=TRUE))
+k_agg <- k_tmp %>% rowwise() %>% mutate(re_suitable_1_ha_qld = sum(across(contains("re1_area")), na.rm=TRUE),
+                                        habitat_present_1_qld = sum(across(contains("re1_habitat")), na.rm=TRUE),
+                                        re_suitable_2_ha_qld = sum(across(contains("re2_area")), na.rm=TRUE),
+                                        habitat_present_2_qld = sum(across(contains("re2_habitat")), na.rm=TRUE),
+                                        re_suitable_3_ha_qld = sum(across(contains("re3_area")), na.rm=TRUE),
+                                        habitat_present_3_qld = sum(across(contains("re3_habitat")), na.rm=TRUE))
 
+k_fix <- k_fix %>% select(!c(re_suitable_12_ha_qld, re_suitable_1_ha_qld, re_suitable_3_ha_qld, habitat_present_3_qld))
 k_fix <- k_fix %>% left_join(k_agg, by='cellid')
 
 ###################################
-save(k_fix, file=paste0(oupdir, "koala_gridded_vars_100ha_tidy.Rdata"))
-st_write(k_fix, paste0(oupdir, "koala_gridded_vars_100ha_tidy.gpkg")) 
+save(k_fix, file=paste0(oupdir, "koala_gridded_vars_100ha_tidy_v2.Rdata"))
+#st_write(k_fix, paste0(oupdir, "koala_gridded_vars_100ha_tidy.gpkg")) 
 
